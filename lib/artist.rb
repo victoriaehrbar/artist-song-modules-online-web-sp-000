210b11d3d97b1ba2febea664bac37b3d332b1da3
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-10 03:26:12 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Artist
   attr_accessor :name
   attr_reader :songs
@@ -19,24 +17,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
